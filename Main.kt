37540f6cf60cf2562b190c73605f6c83c813ddb2
Busra Aydin <56473075+bbaydn@users.noreply.github.com>
2024-03-22 16:55:34 +0300

    90 pages

--- Main.kt
+++ Main.kt
@@ -1,3 +1,6 @@
+import java.io.File
+import java.util.Locale
+
 fun main() {
     // Page 9
     println("Hello, This is Busra.")
@@ -150,8 +153,230 @@ fun main() {
         println("There is no Lily!")
     }
 
+    // Page 42 - Page 43
+    val friendList = listOf("Enes", "Mehmet", "Demet")
+    val friendMap = mapOf("Enes" to 1, "Mehmet" to 2, "Demet" to 3)
+    println(friendMap)
+
+    // Page 44
+    for ((k,v) in friendMap) {
+        println("$k -> $v")
+    }
+
+    // Page 45
+    val lazyStudentInfo: String by lazy {
+        println("First time printed value")
+        "Demet is the lazy student"
+    }
+    println(lazyStudentInfo)
+    println(" ")
+    println(lazyStudentInfo)
+
+    // Page 46
+    fun String.toUpperCaseExt(): String {
+        return this.uppercase()
+    }
+    println("busraakbas".toUpperCaseExt())
+
+    // Page 47
+    Television.findProgram()
+    Television.watch()
+
+    // Page 48
+    val watchList = Classics()
+    watchList.scarface()
+
+    // Page 49
+    val files = File("Test").listFiles()
+    println(files?.size)
+
+    // Page 50
+    println(files?.size ?: "empty")
+
+    val sizeOfFile = files?.size ?: run {
+        val sizeOfThis = getSeven()
+        sizeOfThis * 2
+    }
+    println(sizeOfFile)
+
+    // Page 51
+    val nameOfFriend = friendMap["Merve"] ?: println("There is no Merve in this MapList")
+
+    // Page 52
+    val myFriends = listOf<String>("Mehmet", "Ayla", "Enes", "Merve")
+    val isThatMyFriend = myFriends.firstOrNull() ?: println("Not my friend!")
+
+    // Page 53
+    val value = "BUSRA"
+    value?.let {
+        "No, I don't want to share my informations."
+    }
+
+    // Page 54
+    val name: String? = "Busra"
+    val mapped = name?.let {
+        println("Name area is not null and name is $it")
+        it.length
+    } ?: 0
+    println(mapped)
+
+    // Page 55
+    println(intToColor(3))
+
+    // Page 56
+    println(crypted("BUSRA"))
+
+    // Page 58
+    val ba = if (a == 1) {
+        "one"
+    } else if(a == 3) {
+        "three"
+    } else if (a == 6) {
+        "six"
+    } else {
+        "No Number"
+    }
+    println(ba)
+
+    // Page 59
+    println(arrayOfMilyons(6).contentToString())
+
+    // Page 60
+    println(getSeventeen())
+    println(getSeventeen2())
+
+    // Page 61 - Page 62
+    val myDog = Dog()
+    with(myDog) {
+        bark()
+        playWithKitties()
+    }
+
+    val myCuteDog = Dog().apply {
+        ears = "Short"
+        hasLittleNose = true
+    }
+
+    // Page 64
+    println(genericFunction("Generic"))
+
+    // Page 66
+    fun myLittlePony(): String = TODO("take a feedback")
+
+    // Page 67
+    printMessageWithPrefix("Hello")
+    printMessageWithPrefix(prefix = "Log", s = "Hello")
+
+    // Page 68
+    println("First infix fun: 3 shl 7 = " + (3 shl 7))
+    println("Second infix fun: 4 shr 3 = " + (7 shr 11))
+
+    // Page 70
+    greetings("Hi", "Hello", "Hola", "Merhaba", "Essalamun Aleykum", "Salut")
+
+    // Page 72
+    var aNewVariable: String? = "This is a great coincident"
+    aNewVariable = null
+
+    // Page 73
+    println(description("majezik"))
+
+    // Page 74
+    val customer = Customer()
+    val contact = Contact(1, "This is a customer")
+    println(contact.id)
+    contact.sign = "I am a manager in this place"
+    println(contact.sign)
+
+    // Page 76
+    val shopify = myMutableStack(3.7, 2.2, 3, 2.1)
+    println("Should I buy it = $shopify")
+
+    // Page 77
+    val cylinderPipe = CylinderPipe(1,3)
+    cylinderPipe.calculation()
+
+    // Page 79
+    val aCat = Cats(4, 2, "Fluffy")
+    aCat.writeTheInfo()
+
+    // Page 82
+    val cookies = mutableListOf("Chocolate chip", "Nut", "Cacoa", "Coffee", "Coconut")
+    for (cookie in cookies) {
+        println("Great!! There is $cookie cookie.")
+    }
+
+    // Page 83
+    while (cookies.size < 5) {
+        println("There is still ${cookies.size} cookie here.")
+        cookies.removeAt(cookies.size - 1)
+    }
+
+    // Page 84
+    val cookiess = mutableListOf("Chocolate chip", "Nut", "Cacoa", "Coffee", "Coconut")
+    var newCookies = 0
+    while (cookiess.size <= 5 && cookiess.size != 0) {
+        cookiess.removeAt(cookiess.size - 1)
+        newCookies ++
+        println("We need to bake $newCookies new cookies.")
+    }
+    /*do {
+        if(cookiess.size != 0) {
+            cookiess.removeAt(cookiess.size - 1)
+            newCookies ++
+        }
+    } while (cookiess.size < newCookies)*/
+
+    // Page 85
+    for (i in 1..12) {
+        print(i)
+    }
+    println("")
+    for (i in 9 downTo 0 step 3) {
+        print(i)
+    }
+    println("")
+    for (i in 2..12 step 4) {
+        print(i)
+    }
+
+    println("")
+    // PAGE 86
+    for(g in 'a'..'v') {
+        print(g)
+    }
+    println("")
+
+    // PAGE 87
+    val myValue = 2
+    if (myValue in 1..9) {
+        println("The value is in range from 1 to 9.")
+    }
+    println("")
+    val myOtherValue = 12
+    if (myOtherValue !in 1..9) {
+        println("The value is in range from 1 to 9.")
+    }
+    println("")
+
+    // PAGE 88
+    println(a == b)
+    println(a == myValue)
+    println(b == radius)
+
+    val z = a
+    println(a === z)
+
+    // PAGE 89
+    println(findMinValue(-12, 22))
+
+    // PAGE 91
+    val firstBook = MyBooks("Crime and Punishment ", "Fyodor Dostoevsky", 1866)
+
 
 }
+
+
 
 fun sumOfTwoDigits(firstNumber: Int, secondNumber: Int) = println("Sum of two digits is ${firstNumber + secondNumber}.")
 
@@ -161,7 +386,7 @@ class Cylinder (val radius: Int, val h: Int) : Volume() {
     val volumeOfCylinder = 3.14 * radius * radius * h
 }
 
-// Page 18
+// Page 18 - Page 80 - Page 81
 fun pickFlower(obj: Any): String =
     when (obj) {
         1 -> "You should pick Daisy"
@@ -178,3 +403,156 @@ fun helloFriend(
 ) {
     println("$say Friend! This is $name")
 }
+
+// Page 47
+object Television{
+    init{
+        println("Television is initialized.")
+    }
+
+    fun findProgram(){
+        println("Zap all channels.")
+    }
+
+    fun watch(){
+        println("Stop when you find.")
+    }
+}
+
+// Page 48
+abstract class Movies {
+    abstract fun scarface()
+}
+
+class Classics() : Movies() {
+    override fun scarface() {
+        println("I will watch this movie on september.")
+    }
+
+}
+
+// Page 50
+fun getSeven() = 7
+
+// Page 55
+fun intToColor(value: Int): String {
+    return when(value) {
+        1 -> "Red"
+        2 -> "Yellow"
+        3 -> "Blue"
+        4 -> "Green"
+        else -> "There is no such a color!"
+    }
+}
+
+// Page 56
+fun crypted(word: String): String {
+    val result = StringBuilder()
+    try {
+        for (char in word) {
+            when (char) {
+                in 'a'..'y', in 'A'..'Y' -> result.append(char + 1)
+                'z' -> result.append('a')
+                'Z' -> result.append('A')
+                else -> throw IllegalArgumentException("Its not a word!")
+            }
+        }
+    } catch (e: IllegalArgumentException) {
+        return e.message.toString()
+    }
+
+    return result.toString()
+}
+
+// Page 59
+fun arrayOfMilyons(size: Int) : IntArray = IntArray(size).apply { fill(1000000) }
+
+// Page 60
+fun getSeventeen(): Int {
+    return 17
+}
+fun getSeventeen2() = 17
+
+// Page 61
+class Dog() {
+    var hasLittleNose = false
+    var ears = "Long"
+    fun bark() {}
+    fun playWithKitties() {}
+    fun wishCookies() {}
+}
+
+// Page 64
+inline fun <reified T> genericFunction(value: T) {
+    val typeName = T::class.simpleName
+    println("The type of the name is $typeName")
+}
+
+// Page 67
+fun printMessageWithPrefix(s: String, prefix: String = "Info") {
+    println("$prefix : $s" )
+}
+
+// Page 70
+fun greetings(vararg messages: String) {
+    for (m in messages) println(m)
+}
+
+// Page 73
+fun description(medicineName: String?): String {
+    return when(medicineName) {
+        "arveles" -> "pain killer"
+        "majezik" -> "pain killer"
+        "parol" -> "antipyretic"
+        null -> "Not a medicine"
+        else -> ""
+    }
+}
+
+// Page 74
+class Customer()
+
+class Contact(val id: Int, var sign: String)
+
+// Page 75
+class MutableStack<I>(vararg items: I) {
+    private val item = items.toMutableList()
+    fun addItem(items: I) = item.add(items)
+    fun deleteTheItem(): I = item.removeAt(item.size - 1)
+    fun isEmpty() = item.isEmpty()
+    fun size() = item.size
+    
+}
+
+// Page 76
+fun <I> myMutableStack(vararg item: I) = MutableStack(*item)
+
+open class CalculatedVolume() {
+    open fun calculation() {
+        println("Base area times height")
+    }
+}
+class CylinderPipe (val radius: Int, val h: Int) : CalculatedVolume() {
+    val volumeOfCylinder = 3.14 * radius * radius * h
+}
+
+// Page 79
+open class Animals(val feetCount: Int, val earCount: Int, val name: String) {
+    open fun writeTheInfo() {
+        println("$name has $feetCount feet and $earCount ears.")
+    }
+}
+
+class Cats(feetCount: Int, earCount: Int, name: String): Animals(feetCount, earCount, name) {
+    override fun writeTheInfo() {
+        println("$name has $feetCount feet and $earCount ears. They are so adorable.")
+    }
+
+}
+
+
+// PAGE 89
+fun findMinValue(first: Int, second: Int): Int = if (first > second) second else first
+
+data class MyBooks(val name: String, val author: String, val publishedDate: Int)
+
